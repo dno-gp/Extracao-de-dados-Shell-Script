65681358b143fb2cff2d6b01b941af188f4ffa30
dno-gp <dno.sales@gmail.com>
2018-09-14 10:33:33 -0300

    Adição do comando wget

--- codext.sh
+++ codext.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+#######################################################################################################
+#Scrip para download e manipulação de arquivos refentes aos filiados partidários do estado da Paraíba.#
+#######################################################################################################
+
 echo -e "Processo iniciado.\n"
 
-#Download dos arquivos
+#Download dos arquivos no site do tse
 
-#wget 
+for x in $(cat lspartidos.txt) #Substituir o aquivo txt por uma array/lista
+do wget http://agencia.tse.jus.br/estatistica/sead/eleitorado/filiados/uf/filiados_${x}_pb.zip
+done
+
+
 
 cd <diretório A>
 
 ###Descompactar arquivos zip###
-echo "Descompactar arquivos .zip"
+echo "Descompactando arquivos..."
 
 ls *.zip
 echo " "
@@ -24,7 +32,7 @@ rm <arquivo> #Remover arquivos desnecessários
 
 rename 's/a+//' *.csv #Renomear multiplos arquivos
 
-echo "Converter codificação dos arquivos para UTF-8."
+echo "Convertendo codificação dos arquivos para UTF-8."
 
 for x in $(ls *.csv)
 do iconv -f ISO-8859-16 -t UTF-8 $x > _$x
